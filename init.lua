4623a14f9b5325807700ab9c4579d8759088f741
Harry Anderson <harryanderson9@gmail.com>
2021-09-05 20:16:09 +1000

    use lua module syntax

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- require seperate modules
-require 'hrryndrsn/settings'
-require 'hrryndrsn/keymaps'
-require 'hrryndrsn/plugins'
-require 'hrryndrsn/telescope'
-require 'hrryndrsn/nvim-lspconfig'
-require 'hrryndrsn/colorscheme'
+require 'hrryndrsn.settings'
+require 'hrryndrsn.keymaps'
+require 'hrryndrsn.plugins'
+require 'hrryndrsn.telescope'
+require 'hrryndrsn.nvim-lspconfig'
+require 'hrryndrsn.colorscheme'
