58fa57117f8d2cbd67b6c870d36201ed0f28ab89
Harry Anderson <harryanderson9@gmail.com>
2022-01-19 22:39:12 +0000

    back to rust-tools

--- lua/hrryndrsn/colorscheme.lua
+++ lua/hrryndrsn/colorscheme.lua
@@ -1,4 +1,24 @@
 --Colorscheme
 vim.cmd [[colorscheme base16-tomorrow-night]]
-vim.cmd [[call Base16hi("Comment", g:base16_gui09, "", g:base16_cterm09, "", "", "")]]
+vim.cmd [[call Base16hi("Macro", g:base16_gui09, "", g:base16_cterm09, "", "", "")]]
+vim.cmd [[call Base16hi("Type", g:base16_gui09, "", g:base16_cterm09, "", "", "")]]
+-- vim.cmd [[call Base16hi("Function", g:base16_gui05, "", g:base16_cterm05, "", "", "")]]
+vim.cmd [[call Base16hi("Identifier", g:base16_gui05, "", g:base16_cterm05, "", "", "")]]
 -- require('colorbuddy').colorscheme('gruvbuddy')
+--
+-- vim.cmd [[colorscheme forestbones]]
+-- vim.g.forestbones = { solid_line_nr = true, darken_comments = 70 }
+
+-- vim.cmd [[colorscheme duckbones]]
+-- vim.g.duckbones = { solid_line_nr = true, darken_comments = 45 }
+-- --
+-- vim.cmd [[colorscheme neobones]]
+-- vim.g.neobones = { solid_line_nr = true, darken_comments = 45 }
+--
+--
+-- vim.cmd [[colorscheme lake]]
+-- vim.cmd [[colorscheme tender]]
+--
+-- require("github-theme").setup({
+--   theme_style = "dark",
+-- })
