94a2ac5842ada1c5164b8cc0a0a79fa53877ac3c
Harry Anderson <harryanderson9@gmail.com>
2022-05-01 11:07:33 +0100

    update colors

--- lua/hrryndrsn/zord.lua
+++ lua/hrryndrsn/zord.lua
@@ -1,12 +1,12 @@
 local Color, colors, Group, groups, styles = require('colorbuddy').setup()
 
--- colorbody
+-- colors
 Color.new('white',     '#d8d8d8')
 Color.new('red',       '#cc6666')
 Color.new('pink',      '#fef601')
 Color.new('green',     '#99cc99')
-Color.new('yellow',    '#f8fe7a')
-Color.new('blue',      '#81a2be')
+Color.new('yellow',    '#E1E494')
+Color.new('blue',      '#8abeb7')
 Color.new('aqua',      '#8ec07c')
 Color.new('cyan',      '#8abeb7')
 Color.new('purple',    '#8e6fbd')
@@ -16,64 +16,19 @@ Color.new('brown',     '#a3685a')
 Color.new('seagreen',  '#698b69')
 Color.new('turquoise', '#698b69')
 
-Group.new('Constant'        , colors.violet)
 
-Group.new('Function'        , colors.blue, colors.background, styles.NONE)
+-- zord tweaks
+Group.new('Number', colors.green)
+Group.new('Function', colors.blue)
+Group.new('Statement', colors.violet)
+Group.new('Conditional', colors.violet)
+Group.new('Macro', colors.orange)
+Group.new('Include', colors.blue)
+Group.new('Structure', colors.orange)
+Group.new('Type', colors.orange)
+Group.new('Character', colors.orange)
+Group.new('Repeat', colors.violet)
+Group.new('Identifier', colors.white)
+Group.new('Special', colors.white)
+Group.new('Tag', colors.white)
 
-Group.new('Statement'        , colors.violet)
-
-Group.new('Macro'        , colors.blue)
-
-Group.new('Type'        , colors.white)
-
-
-Group.new('Repeat'        , colors.violet)
-
-
-
-	-- *Comment	any comment
-	--
-	-- *Constant	any constant
-	--  String		a string constant: "this is a string"
-	--  Character	a character constant: 'c', '\n'
-	--  Number		a number constant: 234, 0xff
-	--  Boolean	a boolean constant: TRUE, false
-	--  Float		a floating point constant: 2.3e10
-	--
-	-- *Identifier	any variable name
-	--  Function	function name (also: methods for classes)
-	--
-	-- *Statement	any statement
-	--  Conditional	if, then, else, endif, switch, etc.
-	--  Repeat		for, do, while, etc.
-	--  Label		case, default, etc.
-	--  Operator	"sizeof", "+", "*", etc.
-	--  Keyword	any other keyword
-	--  Exception	try, catch, throw
-	--
-	-- *PreProc	generic Preprocessor
-	--  Include	preprocessor #include
-	--  Define		preprocessor #define
-	--  Macro		same as Define
-	--  PreCondit	preprocessor #if, #else, #endif, etc.
-	--
-	-- *Type		int, long, char, etc.
-	--  StorageClass	static, register, volatile, etc.
-	--  Structure	struct, union, enum, etc.
-	--  Typedef	A typedef
-	--
-	-- *Special	any special symbol
-	--  SpecialChar	special character in a constant
-	--  Tag		you can use CTRL-] on this
-	--  Delimiter	character that needs attention
-	--  SpecialComment	special things inside a comment
-	--  Debug		debugging statements
-	--
-	-- *Underlined	text that stands out, HTML links
-	--
-	-- *Ignore		left blank, hidden  |hl-Ignore|
-	--
-	-- *Error		any erroneous construct
-	--
-	-- *Todo		anything that needs extra attention; mostly the
-	-- 		keywords TODO FIXME and XXX
